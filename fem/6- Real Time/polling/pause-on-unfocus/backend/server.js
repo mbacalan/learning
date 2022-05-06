0928c6a248e77d31e9d4de8fb517aa9f5159edb2
Muhammed Bacalan <muhammedbacalan@gmail.com>
2022-05-06 15:10:30 +0300

    (fem): Add Intro to Real Time

---
+++ fem/6- Real Time/polling/pause-on-unfocus/backend/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+import nanobuffer from "nanobuffer";
+import morgan from "morgan";
+
+const app = express();
+const msg = new nanobuffer(50);
+const getMsgs = () => Array.from(msg).reverse();
+
+msg.push({
+  user: "brian",
+  text: "hi",
+  time: Date.now(),
+});
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(express.static("frontend"));
+
+app.get("/poll", function (req, res) {
+  setTimeout(() => {
+    res.json({
+      msg: getMsgs(),
+    })
+  }, 6000)
+});
+
+app.post("/poll", function (req, res) {
+  const { user, text } = req.body;
+
+  msg.push({
+    user,
+    text,
+    time: Date.now(),
+  });
+
+  res.json({
+    status: "ok",
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`listening on http://localhost:${port}`);
